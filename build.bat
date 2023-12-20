88282fc58010d54953013e010859c2873197c709
Joshua Perrett <jperrett256@gmail.com>
2023-12-20 19:25:59 +0000

    now supports various other MSVC utilities as well (link, clang-cl, etc.)

--- build.bat
+++ build.bat
@@ -1,11 +1,21 @@
 @echo off
+
+echo Building cl.exe
 call build_generic.bat debug cl
 call build_generic.bat release cl
 
-rem rem DEBUG
-rem echo.
-rem call "build/cl"
+echo Building clang-cl.exe
+call build_generic.bat debug clang-cl
+call build_generic.bat release clang-cl
 
-rem rem DEBUG
-rem cp "build\cl.exe" ..
-rem cp "build\cl_dbg.exe" ..
+echo Building clang.exe
+call build_generic.bat debug clang
+call build_generic.bat release clang
+
+echo Building clang++.exe
+call build_generic.bat debug clang++
+call build_generic.bat release clang++
+
+echo Building link.exe
+call build_generic.bat debug link
+call build_generic.bat release link
